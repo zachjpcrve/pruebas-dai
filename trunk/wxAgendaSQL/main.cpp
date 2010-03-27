5b64b8e5f842dd84a46172e022083865ae5e30e0
haitike <haitike@779d064d-a567-7f01-b0a5-fff1745a0c06>
2010-03-27 00:58:01 +0000

    wxAgenda: Base definitiva del proyecto.

--- trunk/wxAgendaSQL/main.cpp
+++ trunk/wxAgendaSQL/main.cpp
@@ -1,43 +1,15 @@
-/*********************************************************************
- * Name:      	main.cpp
- * Purpose:   	Implements simple wxWidgets application with GUI.
- * Author:    
- * Created:   
- * Copyright: 
- * License:   	wxWidgets license (www.wxwidgets.org)
- * 
- * Notes:		
- *********************************************************************/
- 
-#include <wx/wx.h>
+#include "main.h"
+#include "FrameAgenda.h"
 
-// application class
-class wxMiniApp : public wxApp
+IMPLEMENT_APP(AgendaApp)
+
+bool AgendaApp::OnInit()
 {
-    public:
-		// function called at the application initialization
-        virtual bool OnInit();
-
-		// event handler for button click
-        void OnClick(wxCommandEvent& event) { GetTopWindow()->Close(); }
-};
-
-IMPLEMENT_APP(wxMiniApp);
-
-bool wxMiniApp::OnInit()
-{
-	// create a new frame and set it as the top most application window
-    SetTopWindow( new wxFrame( NULL, -1, wxT(""), wxDefaultPosition, wxSize( 100, 50) ) );
-
-	// create new button and assign it to the main frame
-    new wxButton( GetTopWindow(), wxID_EXIT, wxT("Click!") );
-
-	// connect button click event with event handler
-    Connect(wxID_EXIT, wxEVT_COMMAND_BUTTON_CLICKED, wxCommandEventHandler(wxMiniApp::OnClick) );
-
-	// show main frame
-    GetTopWindow()->Show();
-
-	// enter the application's main loop
-    return true;
+	FrameAgenda *frame = new FrameAgenda(wxT("HOLA"),wxSize(768, 700));
+	frame->Show(true);
+    SetTopWindow(frame);
+	return true;
 }
+
+
+
---
+++ trunk/wxAgendaSQL/main.h
@@ -0,0 +1,7 @@
+#include <wx/wx.h>
+
+class AgendaApp : public wxApp
+{
+public:
+    virtual bool OnInit();
+};
