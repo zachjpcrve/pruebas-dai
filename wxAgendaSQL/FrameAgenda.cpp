63f1c362332c616d77e4aa7f91fbef72713a7097
haitike <haitike@779d064d-a567-7f01-b0a5-fff1745a0c06>
2010-03-28 16:10:48 +0000

    wxAgenda: Pruebas con sizers que dejo a medias porque tengo que irme. Sustituyo los wxt() por _() que hacen lo mismo, se escriben mas rapido y sirven para traducir. Meto un script bash para hacer commit en consola que te pregunta que archivos subir y cuales no mediante "dialog".

--- wxAgendaSQL/FrameAgenda.cpp
+++ wxAgendaSQL/FrameAgenda.cpp
@@ -3,16 +3,25 @@
 FrameAgenda::FrameAgenda(const wxString& titulo, const wxSize& size)
 	: wxFrame(NULL, wxID_ANY, titulo, wxDefaultPosition, size)
 {
-	panel = new wxPanel(this,wxID_ANY,wxDefaultPosition,wxDefaultSize);
-	
-	stTam = new wxStaticText(panel, wxID_ANY, wxT("Tamaño:"),wxPoint(50,105));
-	eRedimensionar = new wxTextCtrl(panel,wxID_ANY,wxEmptyString, wxPoint(110, 100)); 
-	bRedimensionar = new wxButton(panel, wxID_ANY, wxT("&Aceptar"), wxPoint(190, 100));
-	
-	stNombre = new wxStaticText(panel, wxID_ANY, wxT("&Nombre:"), wxPoint(50,170));
-	tcNombre = new wxTextCtrl(panel,wxID_ANY,wxEmptyString, wxPoint(110, 165));
-	
-	
+	// Lista de colores a usar: http://html-color-codes.info/codigos-de-colores-hexadecimales/
+	wxColour color1;
+	color1.Set(_("#4f5049"));
+
+	// Le he cambiado el color al panel para diferenciarlo de la venta haciendo pruebas.
+	panel = new wxPanel(this,wxID_ANY, wxDefaultPosition, wxSize(200,200));
+	panel->SetBackgroundColour(color1);
+	bRedimensionar = new wxButton(panel, -1/*wxID_ANY*/, _("&Aceptar"));
+	//tcTam = new wxTextCtrl(panel,wxID_ANY,wxEmptyString, wxPoint(110, 100)); 
+	//tcNombre = new wxTextCtrl(panel,wxID_ANY,wxEmptyString, wxPoint(110, 165));
+
+	// Tutorial de Sizers: http://neume.sourceforge.net/sizerdemo/
+	wxBoxSizer *bsLayout1 = new wxBoxSizer(wxHORIZONTAL);
+	bsLayout1->Add(new wxStaticText(panel, -1, _("Tamaño:")), 0, wxEXPAND | wxLeft | wxTOP, 20);
+	bsLayout1->Add(bRedimensionar, 1, wxEXPAND | wxRIGHT | wxLeft | wxTOP, 100);
+	bsLayout1->SetSizeHints(this);
+
+	panel->SetSizer(bsLayout1);
+
 	Centre();
 }
 
