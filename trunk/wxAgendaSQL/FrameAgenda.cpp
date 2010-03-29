3198ffb792f59e6b4f25ed340940723fe5d3686f
algui91 <algui91@779d064d-a567-7f01-b0a5-fff1745a0c06>
2010-03-29 17:35:36 +0000

    wxAgendaSQL:  He hecho diversos cambios, el tipo de layout no es el mismo, y he quitado lo relacionado con el tamaño de la agenda, ya que es mas sencillo al instanciar el objeto agenda crearla con un determinado numero de contactos, y ya se ocupara la clase de redimensionar el tamaño.

--- trunk/wxAgendaSQL/FrameAgenda.cpp
+++ trunk/wxAgendaSQL/FrameAgenda.cpp
@@ -6,23 +6,39 @@ FrameAgenda::FrameAgenda(const wxString& titulo, const wxSize& size)
 	// Lista de colores a usar: http://html-color-codes.info/codigos-de-colores-hexadecimales/
 	wxColour color1;
 	color1.Set(_("#4f5049"));
+	
+	wxFlexGridSizer *fgs = new wxFlexGridSizer(2 ,2, 10, 30); //Layout flexible
+	wxBoxSizer *hbox = new wxBoxSizer(wxHORIZONTAL);
 
 	// Le he cambiado el color al panel para diferenciarlo de la venta haciendo pruebas.
-	panel = new wxPanel(this,wxID_ANY, wxDefaultPosition, wxSize(200,200));
+	panel = new wxPanel(this, -1);
 	panel->SetBackgroundColour(color1);
-	bRedimensionar = new wxButton(panel, -1/*wxID_ANY*/, _("&Aceptar"));
-	//tcTam = new wxTextCtrl(panel,wxID_ANY,wxEmptyString, wxPoint(110, 100)); 
-	//tcNombre = new wxTextCtrl(panel,wxID_ANY,wxEmptyString, wxPoint(110, 165));
-
-	// Tutorial de Sizers: http://neume.sourceforge.net/sizerdemo/
-	wxBoxSizer *bsLayout1 = new wxBoxSizer(wxHORIZONTAL);
-	bsLayout1->Add(new wxStaticText(panel, -1, _("Tamaño:")), 0, wxEXPAND | wxLeft | wxTOP, 20);
-	bsLayout1->Add(bRedimensionar, 1, wxEXPAND | wxRIGHT | wxLeft | wxTOP, 100);
-	bsLayout1->SetSizeHints(this);
-
-	panel->SetSizer(bsLayout1);
+	
+	//Seccion de Insercion de nombre
+	tcNombre = new wxTextCtrl(panel, -1, _("Nombre..."));
+	fgs->Add(new wxStaticText(panel, -1, _("Nombre:")));
+	fgs->Add(tcNombre, 1, wxEXPAND);
+	
+	//Seccion de Apellido
+	tcApellido = new wxTextCtrl(panel, -1, _("Apellidos..."));
+	fgs->Add(new wxStaticText(panel, -1, _("Apellidos:")));
+	fgs->Add(tcApellido, 1, wxEXPAND);
+	
+	//Seccion DNI
+	tcDni = new wxTextCtrl(panel, -1, wxT("DNI"));
+	fgs->Add(new wxStaticText(panel, -1, _("DNI:")));
+	fgs->Add(tcDni, 1, wxEXPAND);
+	
+	fgs->AddGrowableCol(1,1); //Hacemos que se redimensione solo la columna 1
+	
+	//Este boxSizer horizontal lo usamos para poner 15px, de espacio entre la ventana y los widget
+	hbox->Add(fgs, 1, wxALL | wxEXPAND, 15); 
+	
+	hbox->SetSizeHints(this);
+	panel->SetSizer(hbox);
 
 	Centre();
+
 }
 
 /*void Button::OnQuit(wxCommandEvent & WXUNUSED(event))
