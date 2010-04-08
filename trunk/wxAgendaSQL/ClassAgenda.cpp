d8a67dfac3beaecff5e73ae19ab15e5d23168d80
algui91 <algui91@779d064d-a567-7f01-b0a5-fff1745a0c06>
2010-04-08 21:31:36 +0000

    wxAgenda: Juanjo compila porque no has usado la clase agenda, si te fijas en cada terminacion de una funcion, habias puesto }; como si fuera el cierre de una clase xD. He quitado cosas del estilo if(primero==NULL) y la he puesto simplemente if(primero)

--- trunk/wxAgendaSQL/ClassAgenda.cpp
+++ trunk/wxAgendaSQL/ClassAgenda.cpp
@@ -3,24 +3,24 @@
 void Agenda::push(string c_dni,string c_nombre,string c_apellidos,string direccion,string localidad,int telefono1,int telefono2,int telefono3)
 {
 	Contacto* nuevo=new Contacto(c_dni,c_nombre,c_apellidos,direccion,localidad,telefono1,telefono2,telefono3);
-	if(primero==NULL)
-		primero=nuevo;
+	if(!primero) primero=nuevo; //Si no hay ningun contacto, lo metemos el primero
 	else
 	{	
 		Contacto* temp=primero;
-		while(temp!=NULL)
+		while(temp)
 		{
-			if(nuevo->getNombre()>temp->getNombre())
+			if(nuevo->getNombre() > temp->getNombre()) //si el nuevo va detras del temporal.
 			{
 				nuevo->setSiguiente(temp->getSiguiente());
 				temp->setSiguiente(nuevo);
 				return;
 			}
 			else
-				temp=temp->getSiguiente();
+				temp=temp->getSiguiente(); //avanzamos el puntero
 		} 
 	}
-};
+}
+//----------------------------------------------------------------------------
 void Agenda::pop(string id){
 	Contacto* temp=primero;
 	Contacto* temp2;	
@@ -29,5 +29,5 @@ void Agenda::pop(string id){
 		temp=temp->getSiguiente();
 	}
 	temp2->setSiguiente(temp->getSiguiente());
-	delete(temp);
-};
+	delete temp;
+}
